b13a378478faa24389c85d7de02f5ba8a64bb5b5
Steve Mao (beem it) <maochenyan@gmail.com>
2019-12-11 09:21:26 +1100

    more

--- concurrency/Exceptions.hs
+++ concurrency/Exceptions.hs
@@ -9,3 +9,11 @@ instance Exception MyError
 catcher :: IO a -> IO (Maybe a)
 catcher action = fmap Just action `catch` handler
     where handler (MyError msg) = do putStrLn msg; return Nothing
+
+pureCatcher :: a -> IO (Maybe a)
+pureCatcher a = (a `seq` return (Just a))
+                `catch` \(SomeException _) -> return Nothing
+
+seqList :: [a] -> b -> b
+seqList [] = id
+seqList (a : as) = seq a . seqList as
